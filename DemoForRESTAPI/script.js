20c440344c868dd9fc01b83616428962e6fb9e92
Purinsuu <princemauro.ang@bicol-u.edu.ph>
2019-09-06 17:18:14 +0800

    fully functional REST API

--- DemoForRESTAPI/script.js
+++ DemoForRESTAPI/script.js
@@ -62,4 +62,4 @@ function validateCustomer(customer){
     return Joi.validate(customer, schema);
 }
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log("Listening on port ${port}.."));
+app.listen(port, () => console.log("Listening on port " + port + ".."));
